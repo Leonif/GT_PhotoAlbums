a7dfa9a4b0892d22b8f05e02d462098eef70a2ba
Leonid Nifantyev <leonif1@gmail.com>
2018-11-22 22:47:54 +0200

    albums fetched

--- GT_PhotoAlbums/DataLayer/FaceboookAPI.swift
+++ GT_PhotoAlbums/DataLayer/FaceboookAPI.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import FacebookLogin
+import FacebookCore
 
 class FacebookApi: BackendInterface {
     var manager: LoginManager!
@@ -16,14 +17,29 @@ class FacebookApi: BackendInterface {
         self.manager = manager
     }
     
+    func fetchUserAlbums() {
+        let request = GraphRequest(graphPath: "/me/albums",
+                             httpMethod: .GET)
+        request.start { (response, result) in
+            switch result {
+            case let .success(response: response):
+                debugPrint(response)
+            case let .failed(error):
+                debugPrint(error)
+            }
+        }
+    }
+    
+    
     func login(callback: @escaping (BackendResult<Void>) -> Void) {
-        self.manager.logIn(readPermissions: [.publicProfile], viewController: nil) { (result) in
+        self.manager.logIn(readPermissions: [.publicProfile, .userPhotos], viewController: nil) { (result) in
             switch result {
             case .failed(let error):
                 callback(ResultType.failure(.loginError(error.localizedDescription)))
             case .cancelled:
                 callback(ResultType.failure(.loginError("user cancel enter")))
-            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+            case .success:
+                self.fetchUserAlbums()
                 callback(ResultType.success(()))
             }
         }
